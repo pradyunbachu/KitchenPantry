4107ca910a45d7b49490c9052928791b9cd55ab0
pradyunbachu <pradyun.bachu@gmail.com>
2024-06-04 15:50:47 -0400

    first commit

---
+++ PantryManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class PantryManager{
+
+    private static ArrayList<String> pantryItems = new ArrayList<>();
+
+    public static void main (String[] args){
+        
+    }
+
+
+
+}
